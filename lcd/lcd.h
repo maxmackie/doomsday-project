a41a8ad970063190b4bf940571f37eadc6336592
ChrisSluyter <cpsluyter@rogers.com>
2015-03-11 19:57:47 -0400

    Update lcd.h

--- lcd/lcd.h
+++ lcd/lcd.h
@@ -1,10 +1,8 @@
 #ifndef LCD_H
 #define LCD_H_
 
-#include <stdbool.h>
-
 void initLCD();
-void TaskLCD(void *pvParameters);
-void printLCD(char* message, bool interrupt);
+void TaskLCD();
+void printLCD(char* message);
 
 #endif
