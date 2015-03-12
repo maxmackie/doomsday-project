d7efafff6cf4dbfe346eac38035cc87ee3aa8916
ChrisSluyter <cpsluyter@rogers.com>
2015-03-11 20:28:10 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,137 +2,83 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+
 #include <avr/io.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
+#include "lcd.h"
+#include "wifi.h"
+#include "therm.h"
 #include "serial.h"
-#include "sensor.h"
+
 
 extern xComPortHandle xSerialPort;
 
-// Static declarations of the task functions
-static void taskLEDControl (void *pvParameters);
-static void taskThermalSensor (void *pvParameters);
+uint8_t globalTempArray[9];
 
-// The queue used for IPC between the sensor, LCD and LED tasks
-static QueueHandle_t ipc;
+static void Runner (void *pvParameters);
+typedef void (*TASK)(void); // TASK is now the type  �pointer to function�
 
-// Define the structure that represents a single reading from
-// the temperature sensor. The struct has a value for the ambient
-// temperature aswell as an array of values that represent the
-// pixel temperatures
-struct structMsg
+uint8_t MINOR_CYCLE_TIME = 2;
+
+#define TABLE_SIZE 5
+TASK table[] =
 {
-	unsigned long ambient;
-	unsigned long pixels[];
-} msg;
+	    ts_get_temps,
+		TaskLCD,
+		taskTemperatureLED
+};
+
+//#define MINOR_CYCLE_TIME 50 // millisecs
 
 int main(void) __attribute__((OS_main));
 
 int main(void)
 {
+	xSerialPort = xSerialPortInitMinimal(USART0, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+	initThermalSensor();
+	initLCD();
+	initLEDsOutput();
 
-	// Set up the serial port communications channel
-	xSerialPort = xSerialPortInitMinimal(USART0, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX);
+	xTaskCreate(Runner, (const portCHAR *)"TeamMax", 256, NULL, 3, NULL);
 
-	// Display a polite welcome message - we are Canadians after all
-	avrSerialPrint_P(PSTR("\r\n Successfully running Doomsday Project \r\n"));
-
-	// Create the queue from the sensor to the LED and the sensor to the LCD
-	struct structMsg tmp;
-	ipc = xQueueCreate(1, sizeof(tmp));
-	if (ipc == 0) {
-		// The queue was not created and there was an issue
-		avrSerialPrint_P(PSTR("\r\n ERROR: Could not create the IPC queue \r\n"));
-	}
-
-	// Create the LED task
-    xTaskCreate(taskLEDControl, "LED Task", 256, NULL, 3, NULL);
-    // Create the ThermalSensor task
-    xTaskCreate(taskThermalSensor, "Thermal Sensor Task", 256, NULL, 3, NULL);
-
-	// TODO: Modify the default scheduler with our own one
 	vTaskStartScheduler();
 }
 
-// The LED task will peek at the first value in the global queue and update
-// the projected colour based on what it reads
-static void taskLEDControl(void *pvParameters)
+static void Runner (void *pvParameters) // LOOP
 {
-	// The usual stuff
-	(void) pvParameters;
+	(void) pvParameters;;
 	TickType_t xLastWakeTime;
 	xLastWakeTime = xTaskGetTickCount();
+	int i = 0 ;
 
-	// Turn off the LED colours
-	PORTE |= _BV(PORTE3); // green
-	PORTE |= _BV(PORTE5); // blue
-	PORTH |= _BV(PORTH3); // red
+	while (1){
 
-	// Task loop
-	while (1) {
-		struct structMsg *val;
-		if (ipc != 0) {
-			// Attempt to peek at the first value in the queue
-			if (xQueuePeek(ipc, &(val), (TickType_t) 10)) {
-				// calculate the average value
-				// ...
-				unsigned long avg = 0;
-				if (avg < 20) {
-					// turn on blue and turn off others
-					PORTE |= _BV(PORTE3);
-					PORTE &= ~_BV(PORTE5);
-					PORTH |= _BV(PORTH3);
-				} else if (avg < 30) {
-					// turn on green and turn off others
-					PORTE &= ~_BV(PORTE3);
-					PORTE |= _BV(PORTE5);
-					PORTH |= _BV(PORTH3);
-				} else {
-					// turn on red and turn off others
-					PORTE |= _BV(PORTE3);
-					PORTE |= _BV(PORTE5);
-					PORTH &= ~_BV(PORTH3);
-				}
-			}
+		table[i]();
+
+		vTaskDelayUntil( &xLastWakeTime,(MINOR_CYCLE_TIME/portTICK_PERIOD_MS)); // Sets minor cycle time
+
+		if (i < 2){
+			i ++;
+		}
+		else {
+			i = 0;
 		}
 	}
 }
 
-static void taskThermalSensor(void *pvParameters)
+void vApplicationStackOverflowHook( TaskHandle_t xTask,
+		portCHAR *pcTaskName )
 {
-    (void)pvParameters;
-
-    //Initialize the thermal sensor, this will also enable interrupts
-    initThermalSensor();
-
-    //create some global array which will be of size 9 and hold the ambient temperatures and the temperatures the pixel 1 through 9 hold
-    uint8_t someGlobalTempArray [9];
-
-    //actual call, notice the passing of the array
-    ts_get_temps((uint8_t *)&someGlobalTempArray);
-
-    //array will now be filled with temperatures.
-    for (int i = 0 ; i < NUM_TEMPS; i++){
-        avrSerialPrintf_P(PSTR("\r\nTemp: %u \r\n "), *(someGlobalTempArray + i));
-    }
-
-
-}
-
-// Handles stack overflows by turning off the LEDs and logging an error
-void vApplicationStackOverflowHook(TaskHandle_t xTask, portCHAR *pcTaskName)
-{
-	// Turn off the LEDs
-	PORTE |= _BV(PORTE3); // green
-	PORTE |= _BV(PORTE5); // blue
-	PORTH |= _BV(PORTH3); // red
-
-	// Print console debugging info if we have overflow
-	avrSerialPrint_P(PSTR("\r\n ERROR: Overflow \r\n"));
+	//let all the LEDs be on if overflow occurs
+	/* DDRE  |= _BV(DDE3);
+	PORTE |= _BV(PORTE3);
+	DDRE  |= _BV(DDE5);
+	PORTE |= _BV(PORTE5);
+	DDRH  |= _BV(DDH3);
+	PORTH |= _BV(PORTH3); */
 	while(1);
 }
-
-
