58cab222a5b8f8ddebd823e2e16bc8600955fd21
Max Mackie <github@f33r.com>
2015-03-26 09:02:57 -0400

    new header for lab3

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -9,84 +9,93 @@
 extern uint8_t globalTempArray[9];
 
 /*-----------------------------------------------------------------*
- * Function: initLEDsOutput                                        *
+ * Function: initLEDs                                              *
  * Returns:  none                                                  *
- * Desc:	 Sets the DDR for the LEDs to output                     *
+ * Desc:   Sets the DDR for the LEDs to output                     *
  *-----------------------------------------------------------------*/
-void initLEDsOutput() {
-	DDRH |= _BV(DDH3); // red
-	DDRE |= _BV(DDE5); // blue
-	DDRE |= _BV(DDE3); // green
+void initLED() {
+  DDRH |= _BV(DDH3); // red
+  DDRE |= _BV(DDE5); // blue
+  DDRE |= _BV(DDE3); // green
 }
 
 /*-----------------------------------------------------------------*
  * Function: toggleRed                                             *
  * Returns:  none                                                  *
- * Desc:	 Sets the red LED to ON (1) or OFF (0)                   *
+ * Desc:   Sets the red LED to ON (1) or OFF (0)                   *
  *-----------------------------------------------------------------*/
 void toggleRed(int state) {
-	if (state == 0) {
-		PORTH |= _BV(PORTH3);
-	} else {
-		PORTH &= ~_BV(PORTH3);
-	}
+  if (state == 0) {
+    PORTH |= _BV(PORTH3);
+  } else {
+    PORTH &= ~_BV(PORTH3);
+  }
 }
 
 /*-----------------------------------------------------------------*
  * Function: toggleBlue                                            *
  * Returns:  none                                                  *
- * Desc:	 Sets the blue LED to ON (1) or OFF (0)                  *
+ * Desc:   Sets the blue LED to ON (1) or OFF (0)                  *
  *-----------------------------------------------------------------*/
 void toggleBlue(int state) {
-	if (state == 0) {
-		PORTE |= _BV(PORTE5);
-	} else {
-		PORTE &= ~_BV(PORTE5);
-	}
+  if (state == 0) {
+    PORTE |= _BV(PORTE5);
+  } else {
+    PORTE &= ~_BV(PORTE5);
+  }
 }
 
 /*-----------------------------------------------------------------*
  * Function: toggleGreen                                           *
  * Returns:  none                                                  *
- * Desc:	 Sets the green LED to ON (1) or OFF (0)                 *
+ * Desc:   Sets the green LED to ON (1) or OFF (0)                 *
  *-----------------------------------------------------------------*/
 void toggleGreen(int state) {
-	if (state == 0) {
-		PORTE |= _BV(PORTE3);
-	} else {
-		PORTE &= ~_BV(PORTE3);
-	}
+  if (state == 0) {
+    PORTE |= _BV(PORTE3);
+  } else {
+    PORTE &= ~_BV(PORTE3);
+  }
 }
 
 /*-----------------------------------------------------------------*
  * Function: taskTemperatureLED                                    *
  * Returns:  none                                                  *
- * Desc:	 Takes a pointer to an array of 8 uint8_t values         *
- *			 representing the 8 pixel temperature values. These        *
- *			 values are averaged and the LED colour is changed.	       *
+ * Desc: Takes a pointer to an array of 8 uint8_t values           *
+ *       representing the 8 pixel temperature values. These        *
+ *       values are averaged and the LED colour is changed.        *
  *-----------------------------------------------------------------*/
-void taskTemperatureLED() {
-	while (1) {
-		// calculate the average of the pixel temperatures
-		uint8_t total = 0;
-		for (int i = 0; i < 8; i++) { total += globalTempArray[i]; }
-		float avg = total / 8;
+void taskTemperatureLED(uint8_t temperatureArray[9]) {
+  while (1) {
+    // calculate the average of the pixel temperatures
+    uint8_t total = 0;
+    for (int i = 0; i < 8; i++) { total += temperatureArray[i]; }
+    float avg = total / 8;
 
-		if (avg < 20.00) {
-			// turn on blue and turn off others
-			toggleRed(0);
-			toggleBlue(1);
-			toggleGreen(0);
-		} else if (avg < 30.00) {
-			// turn on green and turn off others
-			toggleRed(0);
-			toggleBlue(0);
-			toggleGreen(1);
-		} else {
-			// turn on red and turn off others
-			toggleRed(1);
-			toggleBlue(0);
-			toggleGreen(0);
-		}
-	}
+    if (avg < 20 && avg > 10) {
+      // turn on blue and turn off others
+      toggleRed(0);
+      toggleBlue(1);
+      toggleGreen(0);
+    } else if (avg > 30 || avg < 10) {
+      // turn on green and turn off others
+      toggleRed(1);
+      toggleBlue(0);
+      toggleGreen(0);
+    } else {
+      // turn on red and turn off others
+      toggleRed(0);
+      toggleBlue(0);
+      toggleGreen(1);
+    }
+  }
+}
+
+/*-----------------------------------------------------------------*
+ * Function: taskMovementLED                                       *
+ * Returns:  none                                                  *
+ * Desc: Updates the LED based on how the robot is moving          *
+ *-----------------------------------------------------------------*/
+void taskMovementLED(uint8_t movementArray[9]) {
+	return;
 }
--- wifi/wifi.h
+++ wifi/wifi.h
@@ -7,11 +7,27 @@
 #ifndef INCLUDE_WIFI_H_
 #define INCLUDE_WIFI_H_
 
-// Prototypes
-void initLEDsOutput(void);
+/*---------------------------------------------*
+ * Update LED DDR to set them as outputs       *
+ *---------------------------------------------*/
+void initLED(void);
+
+/*---------------------------------------------*
+ * Toggle the associated colour LED to either  *
+ * on (state=1) or off (state=0)               *
+ *---------------------------------------------*/
 void toggleRed(int state);
 void toggleBlue(int state);
 void toggleGreen(int state);
-void taskTemperatureLED(void);
+
+/*---------------------------------------------*
+ * Task for changing based on wheel speed      *
+ *---------------------------------------------*/
+void taskMovementLED(uint8_t movementArray[9]);
+
+/*---------------------------------------------*
+ * Task for changing based on temperature      *
+ *---------------------------------------------*/
+void taskTemperatureLED(uint8_t temperatureArray[9]);
 
 #endif /* INCLUDE_WIFI_H_ */
