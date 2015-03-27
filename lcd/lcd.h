af892e744fe7409c22a203f7235ebc69355102b9
ChrisSluyter <cpsluyter@rogers.com>
2015-03-26 20:27:25 -0400

    Update lcd.h

--- lcd/lcd.h
+++ lcd/lcd.h
@@ -1,8 +1,30 @@
 #ifndef LCD_H
 #define LCD_H_
 
-void initLCD();
-void TaskLCD();
-void printLCD(char* message);
+/*----------------------------------------------------------------------------
+ * Description:
+ * 		Performs all initialization for the LCD
+ *----------------------------------------------------------------------------*/
+void initLCD(void);
+
+/*----------------------------------------------------------------------------
+ * Description:
+ * 		Displays the 8 temperature values (indices 1 through 9) of the given
+ * 		tempArray on the LCD
+ *
+ * Parameters:
+ * 		pvParameters - the array of temperatures to display
+ *----------------------------------------------------------------------------*/
+void taskTemperatureLCD(void *pvParameters);
+
+/*----------------------------------------------------------------------------
+ * Description:
+ *		Displays the average temperature and the average speed on the LCD
+ *
+ * Parameters:
+ * 		pvParameters - pointer to data structure containing temperature
+ * 					   and movement arrays
+ *----------------------------------------------------------------------------*/
+void taskAveragesLCD(void *pvParameters);
 
 #endif
