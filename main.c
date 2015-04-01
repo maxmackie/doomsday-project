ed85d241da5e9d0fc964d8a3bb25bfec43735f55
ChrisSluyter <christopher.sluyter@gmail.com>
2015-04-01 18:27:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+/*----------------------------------------------------------------------------
+ * main.c
+ *----------------------------------------------------------------------------*/
 
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-
 #include <avr/io.h>
 
 #include "FreeRTOS.h"
@@ -13,72 +14,40 @@
 #include "wifi.h"
 #include "therm.h"
 #include "serial.h"
+#include "data.h"
 
-
+// USART Serial Port Handle
 extern xComPortHandle xSerialPort;
 
-uint8_t globalTempArray[9];
+// Data object with temperature and speed arrays
+Data data;
 
-static void Runner (void *pvParameters);
-typedef void (*TASK)(void); // TASK is now the type  �pointer to function�
-
-uint8_t MINOR_CYCLE_TIME = 2;
-
-#define TABLE_SIZE 5
-TASK table[] =
-{
-	    ts_get_temps,
-		TaskLCD,
-		taskTemperatureLED
-};
-
-//#define MINOR_CYCLE_TIME 50 // millisecs
-
+/*----------------------------------------------------------------------------
+ * Description:
+ * 		Main function that initializes the modules and creates/runs the tasks
+ *----------------------------------------------------------------------------*/
 int main(void) __attribute__((OS_main));
-
-int main(void)
+int main()
 {
-	xSerialPort = xSerialPortInitMinimal(USART0, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+	// Initialize serial port handle
+	xSerialPort = xSerialPortInitMinimal(USART0, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX);
+
+	// Initialize all the modules
 	initThermalSensor();
+	initLED();
 	initLCD();
-	initLEDsOutput();
 
-	xTaskCreate(Runner, (const portCHAR *)"TeamMax", 256, NULL, 3, NULL);
+	// Create the tasks
+	xTaskCreate(ts_get_temps, (const portCHAR *)"Thermal", 256, (void *)data.temperatures, 3, NULL);
+	xTaskCreate(taskMovementLED, (const portCHAR *)"LED", 256, (void *)&data.move, 3, NULL);
+	xTaskCreate(taskAveragesLCD, (const portCHAR *)"LCD", 256, (void *)&data, 3, NULL);
 
+	// Start the scheduler
 	vTaskStartScheduler();
 }
 
-static void Runner (void *pvParameters) // LOOP
-{
-	(void) pvParameters;;
-	TickType_t xLastWakeTime;
-	xLastWakeTime = xTaskGetTickCount();
-	int i = 0 ;
-
-	while (1){
-
-		table[i]();
-
-		vTaskDelayUntil( &xLastWakeTime,(MINOR_CYCLE_TIME/portTICK_PERIOD_MS)); // Sets minor cycle time
-
-		if (i < 2){
-			i ++;
-		}
-		else {
-			i = 0;
-		}
-	}
-}
-
-void vApplicationStackOverflowHook( TaskHandle_t xTask,
-		portCHAR *pcTaskName )
-{
-	//let all the LEDs be on if overflow occurs
-	/* DDRE  |= _BV(DDE3);
-	PORTE |= _BV(PORTE3);
-	DDRE  |= _BV(DDE5);
-	PORTE |= _BV(PORTE5);
-	DDRH  |= _BV(DDH3);
-	PORTH |= _BV(PORTH3); */
-	while(1);
-}
+/*----------------------------------------------------------------------------
+ * Description:
+ * 		...
+ *----------------------------------------------------------------------------*/
+void vApplicationStackOverflowHook(TaskHandle_t xTask, portCHAR *pcTaskName) { while(1); }
